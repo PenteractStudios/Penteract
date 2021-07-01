60116dbf622e988c39869e045c8e0dfec54e66c2
Álvaro Hinojal <AHinojal@users.noreply.github.com>
2021-07-01 23:12:23 +0200

    feat: improve w/ different movement and fade out

--- Penteract/Assets/Scripts/SceneTransition.cpp
+++ Penteract/Assets/Scripts/SceneTransition.cpp
@@ -4,10 +4,16 @@
 #include "GameObject.h"
 #include "GameplaySystems.h"
 
+#define FADE_SPEED 0.01f
+#define CENTER_POSITION 0.f
+#define ALPHA_TRANSPARENCY 255.f
+#define FULL_TRANSPARENCY 1.f
+
 EXPOSE_MEMBERS(SceneTransition) {
-	MEMBER(MemberType::GAME_OBJECT_UID, transitionUID),
 	MEMBER(MemberType::SCENE_RESOURCE_UID, sceneUID),
-	MEMBER(MemberType::INT, speedTransition)
+	MEMBER(MemberType::GAME_OBJECT_UID, transitionUID),
+	MEMBER(MemberType::INT, transitionMove),
+	MEMBER(MemberType::FLOAT, speedTransition),
 };
 
 GENERATE_BODY_IMPL(SceneTransition);
@@ -16,26 +22,47 @@ void SceneTransition::Start() {
 	transitionGO = GameplaySystems::GetGameObject(transitionUID);
 	if (transitionGO) {
 		transform2D = transitionGO->GetComponent<ComponentTransform2D>();
+		image2D = transitionGO->GetComponent<ComponentImage>();
 	}
 }
 
 void SceneTransition::Update() {
 	if (startTransition) {
 		if (finishedTransition) {
-			if (sceneUID != 0) SceneManager::ChangeScene(sceneUID);
-			if (Time::GetDeltaTime() == 0.f) Time::ResumeGame();
+			if (sceneUID != 0) {
+				isExit ? SceneManager::ExitGame() : SceneManager::ChangeScene(sceneUID);
+				if (Time::GetDeltaTime() == 0.f) Time::ResumeGame();
+			}
 		}
 		else {
 			// Transition
 			// Size should be equal then size of the screen
-			transform2D->SetPosition(float3(transform2D->GetPosition().x + (1.f * speedTransition), 0.f, 0.f));
-			// transform2D->SetPosition(float3(0.f, 0.f, 0.f));
-			finishedTransition = transform2D->GetPosition().x == 0.f;
+			if (transitionMove == (int)TransitionMove::LEFT_TO_RIGHT) {
+				transform2D->SetPosition(float3(transform2D->GetPosition().x + (1.f * speedTransition), 0.f, 0.f));
+				finishedTransition = transform2D->GetPosition().x == CENTER_POSITION;
+			} else if (transitionMove == (int)TransitionMove::RIGHT_TO_LEFT) {
+				transform2D->SetPosition(float3(transform2D->GetPosition().x - (1.f * speedTransition), 0.f, 0.f));
+				finishedTransition = transform2D->GetPosition().x == CENTER_POSITION;
+			} else if (transitionMove == (int)TransitionMove::TOP_TO_BOTTOM) {
+				transform2D->SetPosition(float3(0.f, transform2D->GetPosition().y - (1.f * speedTransition), 0.f));
+				finishedTransition = transform2D->GetPosition().y == CENTER_POSITION;
+			} else if (transitionMove == (int)TransitionMove::BOTTOM_TO_TOP) {
+				transform2D->SetPosition(float3(0.f, transform2D->GetPosition().y + (1.f * speedTransition), 0.f));
+				finishedTransition = transform2D->GetPosition().y == CENTER_POSITION;
+			} else if (transitionMove == (int)TransitionMove::NONE) {
+				if (image2D) {
+					image2D->SetColor(float4(image2D->GetColor().xyz(), image2D->GetColor().w + (FADE_SPEED * speedTransition)));
+					finishedTransition = image2D->GetColor().w >= FULL_TRANSPARENCY;
+				} else {
+					finishedTransition = true;
+				}
+			}
 		}
 	}
 }
 
-void SceneTransition::StartTransition()
+void SceneTransition::StartTransition(bool isExit_)
 {
+	isExit = isExit_;
 	startTransition = true;
 }
--- Penteract/Assets/Scripts/SceneTransition.h
+++ Penteract/Assets/Scripts/SceneTransition.h
@@ -4,30 +4,41 @@
 
 class GameObject;
 class ComponentTransform2D;
+class ComponentImage;
 
 class SceneTransition : public Script
 {
 	GENERATE_BODY(SceneTransition);
 
 public:
+	enum class TransitionMove {
+		NONE,
+		LEFT_TO_RIGHT,
+		RIGHT_TO_LEFT,
+		TOP_TO_BOTTOM,
+		BOTTOM_TO_TOP,
+	};
 
 	void Start() override;
 	void Update() override;
 
-	void StartTransition();
+	void StartTransition(bool isExit_ = false);
 
 public:
 
 	UID sceneUID = 0;
 	UID transitionUID = 0;
 
-	int speedTransition = 10;
+	int transitionMove = (int) TransitionMove::LEFT_TO_RIGHT;
+	float speedTransition = 20;
 
 private:
 	GameObject* transitionGO = nullptr;
 	ComponentTransform2D* transform2D = nullptr;
+	ComponentImage* image2D = nullptr;
 
 	bool startTransition = false;
 	bool finishedTransition = false;
+	bool isExit = false;
 };
 
