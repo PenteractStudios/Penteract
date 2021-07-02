95592f05f2a2e3acf16ee2e2766bdb201b9e3096
FrancPS <fportasol@gmail.com>
2021-07-02 03:02:54 +0200

    DialogueManager COMPLETED

--- Penteract/Assets/Scripts/DialogueManager.cpp
+++ Penteract/Assets/Scripts/DialogueManager.cpp
@@ -6,15 +6,16 @@
 #include "Components/UI/ComponentText.h"
 
 EXPOSE_MEMBERS(DialogueManager) {
-	// Add members here to expose them to the engine. Example:
-	// MEMBER(MemberType::BOOL, exampleMember1),
-	// MEMBER(MemberType::PREFAB_RESOURCE_UID, exampleMember2),
 	MEMBER(MemberType::GAME_OBJECT_UID, fangTextObjectUID),
-		MEMBER(MemberType::GAME_OBJECT_UID, onimaruTextObjectUID),
-		MEMBER(MemberType::FLOAT3, startPosition),
-		MEMBER(MemberType::FLOAT3, endPosition),
-		MEMBER(MemberType::FLOAT, appearAnimationTime),
-		MEMBER(MemberType::FLOAT, disappearAnimationTime)
+	MEMBER(MemberType::GAME_OBJECT_UID, onimaruTextObjectUID),
+	MEMBER(MemberType::GAME_OBJECT_UID, tutorialFangUID),
+	MEMBER(MemberType::GAME_OBJECT_UID, tutorialOnimaruUID),
+	MEMBER(MemberType::FLOAT3, dialogueStartPosition),
+	MEMBER(MemberType::FLOAT3, dialogueEndPosition),
+	MEMBER(MemberType::FLOAT3, tutorialStartPosition),
+	MEMBER(MemberType::FLOAT3, tutorialEndPosition),
+	MEMBER(MemberType::FLOAT, appearAnimationTime),
+	MEMBER(MemberType::FLOAT, disappearAnimationTime)
 };
 
 GENERATE_BODY_IMPL(DialogueManager);
@@ -22,38 +23,45 @@ GENERATE_BODY_IMPL(DialogueManager);
 void DialogueManager::Start() {
 	GameObject* fangTextObject = GameplaySystems::GetGameObject(fangTextObjectUID);
 	GameObject* onimaruTextObject = GameplaySystems::GetGameObject(onimaruTextObjectUID);
+	tutorialFang = GameplaySystems::GetGameObject(tutorialFangUID);
+	tutorialOnimaru = GameplaySystems::GetGameObject(tutorialOnimaruUID);
+
 	if (fangTextObject && onimaruTextObject) {
 		fangTextComponent = fangTextObject->GetComponent<ComponentText>();
 		onimaruTextComponent = onimaruTextObject->GetComponent<ComponentText>();
-		RetrieveUIComponents(fangTextComponent->GetOwner().GetParent());
-
-		if (fangTextComponent && onimaruTextComponent) {
-			dialoguesArray[0] = Dialogue(1, "Hi, I'm Fang!", &dialoguesArray[1]);
-			dialoguesArray[1] = Dialogue(2, "And I'm Onimaru", &dialoguesArray[2]);
-			dialoguesArray[2] = Dialogue(2, "N' we gonna kik yur arse!", nullptr);
-		}
 	}
+
+	// SWAP DIALOGUE + ONIMARU TUTORIAL
+	dialoguesArray[0] = Dialogue(1, "Hi, I'm Fang!\n akljafddoadf\ndlskds\nldsjds", &dialoguesArray[1]);
+	dialoguesArray[1] = Dialogue(2, "And I'm Onimaru", &dialoguesArray[2]);
+	dialoguesArray[2] = Dialogue(2, "N' we gonna kik yur arse!", &dialoguesArray[3]);
+	dialoguesArray[3] = Dialogue(5, "", &dialoguesArray[4]);
+	dialoguesArray[4] = Dialogue(2, "And I'm Onimaru", nullptr);
+
 }
 
 void DialogueManager::Update() {
 	if (!fangTextComponent) return;
 	if (!onimaruTextComponent) return;
-
-	// SWAP DIALOGUE + TUTORIAL
-	if (triggerSwapDialogues) {
-
-	}
-
+	if (!tutorialFang) return;
+	if (!tutorialOnimaru) return;
 
 	if (activeDialogue) {
 		if (runOpenAnimation) ActivateDialogue(activeDialogue);
 
-		if (Input::GetKeyCodeDown(Input::KEY_F) && !(runOpenAnimation || runChangeAnimation)) {
-			if (activeDialogue->nextDialogue) runChangeAnimation = true;
+		if (Input::GetKeyCodeDown(Input::KEY_F) && !(runOpenAnimation || runChangeAnimation || runCloseAnimation) && activeDialogueObject) {
+			if (activeDialogue->nextDialogue) {
+				runChangeAnimation = true;
+				if ((activeDialogue->character < 5 && activeDialogue->nextDialogue->character >= 5) ||
+					(activeDialogue->character >= 5 && activeDialogue->nextDialogue->character < 5)) {
+					runCloseAnimation = true;
+					runSecondaryOpen = true;
+				}
+			}
 			else runCloseAnimation = true;
 		}
 
-		if (runChangeAnimation) {
+		if (runChangeAnimation && !runCloseAnimation) {
 			ActivateNextDialogue(activeDialogue);
 		}
 
@@ -64,6 +72,15 @@ void DialogueManager::Update() {
 void DialogueManager::SetActiveDialogue(Dialogue* dialogue, bool runAnimation)
 {
 	if (dialogue) {
+		if (dialogue->character >= 5) {
+			currentStartPosition = tutorialStartPosition;
+			currentEndPosition = tutorialEndPosition;
+		}
+		else {
+			currentStartPosition = dialogueStartPosition;
+			currentEndPosition = dialogueEndPosition;
+		}
+
 		activeDialogue = dialogue;
 		if (dialogue->character == 1) {          // FANG
 			activeDialogueObject = fangTextComponent->GetOwner().GetParent();
@@ -75,15 +92,25 @@ void DialogueManager::SetActiveDialogue(Dialogue* dialogue, bool runAnimation)
 		}
 		else if (dialogue->character == 3) {     // DUKE
 		}
+		else if (dialogue->character == 4) {     // ROSAMONDE
+		}
+		else if (dialogue->character == 5) {     // TUTORIAL
+			activeDialogueObject = tutorialFang;
+		}
+		else if (dialogue->character == 6) {     // TUTORIAL
+			activeDialogueObject = tutorialOnimaru;
+		}
 		runOpenAnimation = runAnimation;
+		RetrieveUIComponents(activeDialogueObject);
 	}
 	else {
-		activeDialogue = nullptr;
+		//activeDialogue = nullptr;
 		activeDialogueObject = nullptr;
 	}
 }
 
 void DialogueManager::ActivateDialogue(Dialogue* dialogue) {
+	runSecondaryOpen = false;
 
 	if (activeDialogueObject) {
 		if (!activeDialogueObject->IsActive()) {
@@ -95,13 +122,13 @@ void DialogueManager::ActivateDialogue(Dialogue* dialogue) {
 
 	if (runOpenAnimation) {
 		if (animationLerpTime < appearAnimationTime) {
-			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(float3::Lerp(startPosition, endPosition, animationLerpTime / appearAnimationTime));
+			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(float3::Lerp(currentStartPosition, currentEndPosition, animationLerpTime / appearAnimationTime));
 			TransitionUIElementsColor(true);
 		}
 		else {
 			runOpenAnimation = false;
 			animationLerpTime = 0;
-			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(endPosition);
+			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(currentEndPosition);
 			TransitionUIElementsColor(true, false);
 		}
 	}
@@ -109,13 +136,17 @@ void DialogueManager::ActivateDialogue(Dialogue* dialogue) {
 
 void DialogueManager::ActivateNextDialogue(Dialogue* dialogue)
 {
-	activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(startPosition);
+	activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(currentStartPosition);
 	activeDialogueObject->Disable();
-
 	// here should go the transition animations
 	SetActiveDialogue(dialogue->nextDialogue, false);
 	activeDialogueObject->Enable();
-	activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(endPosition);
+	if (runSecondaryOpen) {
+		runOpenAnimation = true;
+	}
+	else {
+		activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(currentEndPosition);
+	}
 	runChangeAnimation = false;
 }
 
@@ -124,22 +155,21 @@ void DialogueManager::CloseDialogue(Dialogue* dialogue)
 	animationLerpTime += Time::GetDeltaTime();
 	if (runCloseAnimation) {
 		if (animationLerpTime < appearAnimationTime) {
-			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(float3::Lerp(endPosition, startPosition, animationLerpTime / appearAnimationTime));
+			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(float3::Lerp(currentEndPosition, currentStartPosition, animationLerpTime / appearAnimationTime));
 			TransitionUIElementsColor(false);
 		}
 		else {
 			runCloseAnimation = false;
 			animationLerpTime = 0;
-			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(startPosition);
+			activeDialogueObject->GetComponent<ComponentTransform2D>()->SetPosition(currentStartPosition);
 			TransitionUIElementsColor(false, false);
 
-			
 			if (activeDialogueObject) {
 				if (activeDialogueObject->IsActive()) {
 					activeDialogueObject->Disable();
 				}
 			}
-			SetActiveDialogue(nullptr);
+			if (!dialogue->nextDialogue) SetActiveDialogue(nullptr);
 		}
 	}
 }
@@ -164,9 +194,10 @@ void DialogueManager::TransitionUIElementsColor(bool appearing, bool mustLerp)
 
 void DialogueManager::RetrieveUIComponents(GameObject* current)
 {
-	if (current == nullptr) {
-		return;
-	}
+	if (!current) return;
+
+	uiComponents.clear();
+	uiColors.clear();
 
 	ComponentImage* image = current->GetComponent<ComponentImage>();
 	ComponentText* text = current->GetComponent<ComponentText>();
--- Penteract/Assets/Scripts/DialogueManager.h
+++ Penteract/Assets/Scripts/DialogueManager.h
@@ -13,7 +13,7 @@ class DialogueManager : public Script
 		Dialogue(int character, const char* text, Dialogue* nextDialogue);
 		~Dialogue();
 	public:
-		int character = 0; // 1 = Fang, 2 = Onimaru, 3 = Duke, 4 = Rosamonde
+		int character = 0; // 1 = Fang, 2 = Onimaru, 3 = Duke, 4 = Rosamonde, 5 = Tutorial Window
 		const char* text = nullptr;
 		Dialogue* nextDialogue = nullptr;
 	};
@@ -32,7 +32,7 @@ public:
 	void RetrieveUIComponents(GameObject* current);
 
 public:
-	Dialogue dialoguesArray[3];
+	Dialogue dialoguesArray[5];
 	Dialogue* activeDialogue = nullptr;			// Pointer to the Dialogue Tree
 	GameObject* activeDialogueObject = nullptr; // Pointer to the Dialogue GameObject in the scene (DialogueFang, DialogueOnimaru or DialogueDuke)
 
@@ -42,9 +42,18 @@ public:
 	ComponentText* fangTextComponent = 0;
 	ComponentText* onimaruTextComponent = 0;
 
+	UID tutorialFangUID = 0;
+	UID tutorialOnimaruUID = 0;
+	GameObject* tutorialFang = nullptr;
+	GameObject* tutorialOnimaru = nullptr;
+
 	// ------ TRANSITION INFO ------ //
-	float3 startPosition = float3(0, 0, 0);
-	float3 endPosition = float3(0, 0, 0);
+	float3 dialogueStartPosition = float3(0, 0, 0);
+	float3 dialogueEndPosition = float3(0, 0, 0);
+	float3 tutorialStartPosition = float3(0, 0, 0);
+	float3 tutorialEndPosition = float3(0, 0, 0);
+	float3 currentStartPosition = float3(0, 0, 0);
+	float3 currentEndPosition = float3(0, 0, 0);
 	float appearAnimationTime = .5f;
 	float disappearAnimationTime = .5f;
 	float animationLerpTime = 0.0f;
@@ -53,6 +62,7 @@ public:
 	bool runOpenAnimation = false;
 	bool runChangeAnimation = false;
 	bool runCloseAnimation = false;
+	bool runSecondaryOpen = false;
 
 	// ---------- TRIGGERS --------- //
 	bool triggerSwapDialogues = false;
