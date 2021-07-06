158a37255f1113288127b4206ecb75d530af8efb
magalenyo <magalenyo7@gmail.com>
2021-07-07 01:18:29 +0200

    Update VSyncSetter.cpp

--- Penteract/Assets/Scripts/VSyncSetter.cpp
+++ Penteract/Assets/Scripts/VSyncSetter.cpp
@@ -4,10 +4,6 @@
 #include "Components/UI/ComponentToggle.h"
 
 EXPOSE_MEMBERS(VSyncSetter) {
-    // Add members here to expose them to the engine. Example:
-    // MEMBER(MemberType::BOOL, exampleMember1),
-    // MEMBER(MemberType::PREFAB_RESOURCE_UID, exampleMember2),
-    // MEMBER(MemberType::GAME_OBJECT_UID, exampleMember3)
 };
 
 GENERATE_BODY_IMPL(VSyncSetter);
