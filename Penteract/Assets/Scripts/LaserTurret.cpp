0babdda7ceea66d01566677e954eb1565c977965
jordi-gil <jordigilgonzalez@hotmail.com>
2021-07-07 16:49:42 +0200

    First iteration for laser logic

--- Penteract/Assets/Scripts/LaserTurret.cpp
+++ Penteract/Assets/Scripts/LaserTurret.cpp
@@ -1,18 +1,61 @@
 #include "LaserTurret.h"
 
+#include "GameplaySystems.h"
+#include "GameObject.h"
+
 EXPOSE_MEMBERS(LaserTurret) {
-    // Add members here to expose them to the engine. Example:
-    // MEMBER(MemberType::BOOL, exampleMember1),
-    // MEMBER(MemberType::PREFAB_RESOURCE_UID, exampleMember2),
-    // MEMBER(MemberType::GAME_OBJECT_UID, exampleMember3)
+    MEMBER(MemberType::GAME_OBJECT_UID, laserTargetUID)
 };
 
 GENERATE_BODY_IMPL(LaserTurret);
 
 void LaserTurret::Start() {
 	
+    GameObject* owner = &GetOwner();
+
+    if (owner) {
+        animationComp = owner->GetComponent<ComponentAnimation>();
+    }
+
+    laserObject = GameplaySystems::GetGameObject(laserTargetUID);
+    if (laserObject) {
+        laserObject->Disable();
+    }
+
 }
 
 void LaserTurret::Update() {
+
+    if (currentState != TurretState::SHOOT) {
+        if (laserObject && laserObject->IsActive()) laserObject->Disable();
+    }
+    else {
+        if (laserObject && laserObject->IsActive()) laserObject->Enable();
+    }
 	
+}
+
+void LaserTurret::Init() {
+
+    currentState = TurretState::START;
+
+}
+
+void LaserTurret::OnAnimationFinished() {
+
+    if (animationComp && animationComp->GetCurrentState()) {
+
+        if (animationComp->GetCurrentState()->name == states[static_cast<int>(TurretState::START)]) {
+            animationComp->SendTrigger(animationComp->GetCurrentState()->name + states[static_cast<int>(TurretState::SHOOT)]);
+            currentState = TurretState::SHOOT;
+        } else if (animationComp->GetCurrentState()->name == states[static_cast<int>(TurretState::SHOOT)]) {
+            animationComp->SendTrigger(animationComp->GetCurrentState()->name + states[static_cast<int>(TurretState::END)]);
+            currentState = TurretState::END;
+        } else if (animationComp->GetCurrentState()->name == states[static_cast<int>(TurretState::END)]) {
+            animationComp->SendTrigger(animationComp->GetCurrentState()->name + states[static_cast<int>(TurretState::START)]);
+            currentState = TurretState::START;
+        }
+
+    }
+
 }
--- Penteract/Assets/Scripts/LaserTurret.h
+++ Penteract/Assets/Scripts/LaserTurret.h
@@ -2,14 +2,37 @@
 
 #include "Scripting/Script.h"
 
+class ComponentAnimation;
+
+static const char* states[] = { "Start", "Shoot", "End" };
+
 class LaserTurret : public Script
 {
 	GENERATE_BODY(LaserTurret);
+
+	enum class TurretState {
+		START = 0,
+		SHOOT,
+		END
+	};
 
 public:
 
 	void Start() override;
 	void Update() override;
+
+	void Init();
+
+	void OnAnimationFinished() override;
+
+	UID laserTargetUID = 0;
+
+private:
+
+	ComponentAnimation* animationComp = nullptr;
+	TurretState currentState = TurretState::START;
+	
+	GameObject* laserObject = nullptr;
 
 };
 
