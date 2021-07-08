3066a326a34753c0f046471c19488dda4ff3df77
Devildrake <davidsierragonzalez@enti.cat>
2021-07-08 11:27:42 +0200

    Deleted some comments

--- Penteract/Assets/Scripts/UseGamepadSetter.cpp
+++ Penteract/Assets/Scripts/UseGamepadSetter.cpp
@@ -2,10 +2,6 @@
 #include "PlayerController.h"
 
 EXPOSE_MEMBERS(UseGamepadSetter) {
-	// Add members here to expose them to the engine. Example:
-	// MEMBER(MemberType::BOOL, exampleMember1),
-	// MEMBER(MemberType::PREFAB_RESOURCE_UID, exampleMember2),
-	// MEMBER(MemberType::GAME_OBJECT_UID, exampleMember3)
 };
 
 GENERATE_BODY_IMPL(UseGamepadSetter);
