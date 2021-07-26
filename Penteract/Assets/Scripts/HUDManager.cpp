658824fd372dcbd11e7883ffe0e9813cc869d0cd
Devildrake <davidsierragonzalez@enti.cat>
2021-07-26 16:02:05 +0200

    5th Iteration of the HUDSkills prefab, now with proper picto text and colors for the switch skill Preparing some variables for the functioning of the HudManagerScript

--- Penteract/Assets/Scripts/HUDManager.cpp
+++ Penteract/Assets/Scripts/HUDManager.cpp
@@ -1,18 +1,17 @@
 #include "HUDManager.h"
 
 EXPOSE_MEMBERS(HUDManager) {
-    // Add members here to expose them to the engine. Example:
-    // MEMBER(MemberType::BOOL, exampleMember1),
-    // MEMBER(MemberType::PREFAB_RESOURCE_UID, exampleMember2),
-    // MEMBER(MemberType::GAME_OBJECT_UID, exampleMember3)
+	MEMBER(MemberType::GAME_OBJECT_UID, fangSkillParentUID),
+	MEMBER(MemberType::GAME_OBJECT_UID, onimaruSkillParentUID),
+	MEMBER(MemberType::GAME_OBJECT_UID, switchSkillParentUID)
 };
 
 GENERATE_BODY_IMPL(HUDManager);
 
 void HUDManager::Start() {
-	
+
 }
 
 void HUDManager::Update() {
-	
+
 }
--- Penteract/Assets/Scripts/HUDManager.h
+++ Penteract/Assets/Scripts/HUDManager.h
@@ -2,14 +2,36 @@
 
 #include "Scripting/Script.h"
 
-class HUDManager : public Script
-{
+class HUDManager : public Script {
 	GENERATE_BODY(HUDManager);
 
 public:
 
 	void Start() override;
 	void Update() override;
+
+public:
+	//Skill HUD
+	float4 buttonColorNotAvailable = float4(0, 40, 60, 255);
+	float4 buttonColorAvailable = float4(255, 255, 255, 255);
+	float4 buttonTextColorAvailable = float4(0, 0, 0, 255);
+	float4 buttonTextColorAvailable = float4(255, 255, 255, 255);
+	float4 skillColorNotAvailable = float4(0, 93, 145, 255);
+	float4 skillColorAvailable = float4(0, 177, 227, 255);
+	float4 switchSkillColorNotAvailable = float4(133, 243, 196, 150);
+	float4 switchSkillColorAvailable = float4(133, 243, 196, 255);
+	float4 switchPictoColorNotInUse = float4(133, 243, 196, 255);
+	float4 switchPictoColorInUse = float4(0, 0, 0, 255);
+	UID fangSkillParentUID = 0;
+	UID onimaruSkillParentUID = 0;
+	UID switchSkillParentUID = 0;
+
+
+private:
+	//Skill HUD
+	GameObject* fangSkillParent = nullptr;
+	GameObject* onimaruSkillParent = nullptr;
+	GameObject* switchSkillParent = nullptr;
 
 };
 
