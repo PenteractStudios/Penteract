6eacb944d30db4bcf6dfaf29bd6f13ce69eaa617
Isaac Fernandez Fontao <isaacfdez94@gmail.com>
2021-08-06 12:06:20 +0200

    Basic structure for Duke class

---
+++ Penteract/Assets/Scripts/Duke.cpp
@@ -0,0 +1,29 @@
+#include "Duke.h"
+
+void Duke::Init(UID dukeUID, UID playerUID)
+{
+}
+
+void Duke::ShootAndMove(const float3& newPosition)
+{
+}
+
+void Duke::MeleeAttack()
+{
+}
+
+void Duke::ShieldShoot()
+{
+}
+
+void Duke::BulletHell()
+{
+}
+
+void Duke::Charge()
+{
+}
+
+void Duke::CallTroops()
+{
+}
---
+++ Penteract/Assets/Scripts/Duke.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Character.h"
+
+class Duke : public Character
+{
+public:
+	// ------- Contructors ------- //
+	Duke() {}
+
+	Duke(float lifePoints_, float movementSpeed_, float damageHit_, float damageBullet_,float damageCharge_, float searchRadius_, float attackRange_, float timeToDie_, float pushBackDistance_, float pushBackSpeed_)
+		: searchRadius(searchRadius_)
+		, attackRange(attackRange_)
+		, timeToDie(timeToDie_)
+		, pushBackDistance(pushBackDistance_)
+		, pushBackSpeed(pushBackSpeed_)
+		, damageBullet(damageBullet_)
+		, damageCharge(damageCharge_) {
+		lifePoints = lifePoints_;
+		movementSpeed = movementSpeed_;
+		damageHit = damageHit_;
+		SetTotalLifePoints(lifePoints);
+	}
+
+	// ------- Core Functions ------ //
+	void Init(UID dukeUID, UID playerUID);
+	void ShootAndMove(const float3& newPosition);
+	void MeleeAttack();
+	void ShieldShoot();
+	void BulletHell();
+	void Charge();
+	void CallTroops();
+		
+public:
+	float damageBullet = 1.f;
+	float damageCharge = 1.f;
+	float searchRadius = 10.f;
+	float attackRange = 2.0f;
+	float timeToDie = 5.f;
+	float pushBackDistance = 5.f;
+	float pushBackSpeed = 5.f;
+
+private:
+	GameObject* player = nullptr;
+};
