0cf1184b1d351c2738728388f3b018e937ef1b5f
FrancPS <fportasol@gmail.com>
2021-08-12 10:23:22 +0200

    New dialogues scene

---
+++ Penteract/Assets/Scripts/TriggerDialoguePowerUp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Scripting/Script.h"
+
+class GameObject;
+class DialogueManager;
+class PlayerController;
+class TriggerDialoguePowerUp : public Script
+{
+	GENERATE_BODY(TriggerDialoguePowerUp);
+
+public:
+
+	void Start() override;
+	void Update() override;
+	void OnCollision(GameObject& collidedWith, float3 collisionNormal, float3 penetrationDistance, void* particle = nullptr) override;
+
+public:
+	UID gameControllerUID = 0;
+	UID playerUID = 0;
+	GameObject* gameController = nullptr;
+	GameObject* player = nullptr;
+	DialogueManager* dialogueManagerScript = nullptr;
+	PlayerController* playerControllerScript = nullptr;
+};
+
