ec36eebb377fb9195b35926609caf56e4aa320c7
Mefiso <polpegra@gmail.com>
2021-09-05 15:28:46 +0200

    Added pattern for security lasers

---
+++ Penteract/Assets/Scripts/LasersGenerator.cpp
@@ -0,0 +1,85 @@
+#include "LasersGenerator.h"
+
+#include "GameplaySystems.h"
+#include "GameObject.h"
+
+EXPOSE_MEMBERS(LasersGenerator) {
+    MEMBER(MemberType::FLOAT, coolDownOn),
+    MEMBER(MemberType::FLOAT, coolDownOnTimer),
+    MEMBER(MemberType::FLOAT, coolDownOff),
+    MEMBER(MemberType::FLOAT, coolDownOffTimer),
+    MEMBER(MemberType::GAME_OBJECT_UID, laserTargetUID),
+    MEMBER(MemberType::GAME_OBJECT_UID, pairGeneratorUID)
+};
+
+GENERATE_BODY_IMPL(LasersGenerator);
+
+void LasersGenerator::Start() {
+    GameObject* owner = &GetOwner();
+    GameObject* pair = GameplaySystems::GetGameObject(pairGeneratorUID);
+
+    if (owner) {
+        animationComp = owner->GetComponent<ComponentAnimation>();
+    }
+    if (pair) {
+        pairAnimationComp = pair->GetComponent<ComponentAnimation>();
+    }
+
+    laserObject = GameplaySystems::GetGameObject(laserTargetUID);
+    if (laserObject) {
+        laserObject->Disable();
+    }
+}
+
+void LasersGenerator::Update() {
+    if (!animationComp) {
+        Debug::Log("animationComp is nullptr");
+    }
+
+    switch (currentState)
+    {
+    case GeneratorState::IDLE:
+        if (coolDownOffTimer <= coolDownOff) {
+            coolDownOffTimer += Time::GetDeltaTime();
+            if (coolDownOffTimer > coolDownOff) {
+                coolDownOffTimer = 0;
+                currentState = GeneratorState::START;
+                animationComp->SendTrigger(states[static_cast<unsigned int>(GeneratorState::IDLE)] + states[static_cast<unsigned int>(GeneratorState::START)]);
+                //pairAnimationComp->SendTrigger(states[static_cast<unsigned int>(GeneratorState::IDLE)] + states[static_cast<unsigned int>(GeneratorState::START)]);
+            }
+        }
+        break;
+    case GeneratorState::START:
+        if (chargingTimer <= CHARGING_DURATION) {
+            chargingTimer += Time::GetDeltaTime();
+            if (chargingTimer > CHARGING_DURATION) {
+                chargingTimer = 0.f;
+                currentState = GeneratorState::SHOOT;
+            }
+        }
+        break;
+    case GeneratorState::SHOOT:
+        if (coolDownOnTimer <= coolDownOn) {
+            coolDownOnTimer += Time::GetDeltaTime();
+            if (coolDownOnTimer > coolDownOn) {
+                coolDownOnTimer = 0;
+                currentState = GeneratorState::IDLE;
+                animationComp->SendTrigger(states[static_cast<unsigned int>(GeneratorState::SHOOT)] + states[static_cast<unsigned int>(GeneratorState::IDLE)]);
+                //pairAnimationComp->SendTrigger(states[static_cast<unsigned int>(GeneratorState::SHOOT)] + states[static_cast<unsigned int>(GeneratorState::IDLE)]);
+
+            }
+        }
+        break;
+    }
+
+    if (currentState != GeneratorState::SHOOT) {
+        if (laserObject && laserObject->IsActive()) laserObject->Disable();
+    }
+    else {
+        if (laserObject && !laserObject->IsActive()) laserObject->Enable();
+    }
+}
+
+void LasersGenerator::Init() {
+    currentState = GeneratorState::IDLE;
+}
