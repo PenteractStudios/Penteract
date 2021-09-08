53e0bc90b8368a50e4bf901a59a50374d42e6ea6
Isaac Fernandez Fontao <isaacfdez94@gmail.com>
2021-09-08 17:31:23 +0200

    Add new corridor patterns

--- Penteract/Assets/Scripts/TilesPatterns.h
+++ Penteract/Assets/Scripts/TilesPatterns.h
@@ -1,18 +1,32 @@
 #pragma once
 
+#define CORRIDOR_PATTERNS 11
 #define CORRIDOR_TILES 28
 #define ARENA_TILES 32
 #define BOSS_TILES 1
 
-bool corridorPattern1[CORRIDOR_TILES] = { true, false, true, false, true, false, false, false, false, true,
-												false, true, false, true, false, false, true, false, true, false,
-												true, false, false, false, false, true, false, true };
-bool corridorPattern2[CORRIDOR_TILES] = { false, false, false, false, true, true, true, true, false, false,
-											false, false, true, true, true, true, false, false, false, false,
-											true, true, true, true, false, false, false, false };
-bool corridorPattern3[CORRIDOR_TILES] = { true, true, true, true, false, false, false, false, true, true,
-											true, true, false, false, false, false, true, true, true, true,
-											false, false, false, false, true, true, true, true };
+bool corridorPatterns[CORRIDOR_PATTERNS][CORRIDOR_TILES] = { /*Nº 1*/{	false, false, true, true, true, true, true, true, true, true, true, true, true, true,
+																		true, false, true, false, false, false, true, true, true, true, true, true, true, true},
+															 /*Nº 2*/{	false, false, true, false, true, false, true, true, true, true, true, true, true, true,
+																		true, true, true, true, false, false, false, true, false, true, true, true, true, true},
+															 /*Nº 3*/{	true, true, true, false, false, false, false, true, true, true, true, true, true, true,
+																		true, true, true, true, true, true, false, true, false, false, true, false, true, true},
+															 /*Nº 4*/{	true, true, true, true, false, true, false, true, false, true, false, true, true, true,
+																		true, true, true, true, true, true, true, true, true, false, true, false, false, false},
+															 /*Nº 5*/{	true, true, true, true, true, true, true, true, false, true, false, true, false, false,
+																		true, true, true, true, true, true, true, true, true, true, false, true, false, true},
+															 /*Nº 6*/{	false, false, true, true, true, true, true, true, true, false, true, false, true, false,
+																		true, false, true, false, true, true, true, true, true, true, true, true, true, true},
+															 /*Nº 7*/{	false, false, true, false, true, false, true, true, true, true, true, true, true, true,
+																		true, false, true, false, false, false, true, true, true, true, true, true, true, true},
+															 /*Nº 8*/{	true, true, true, false, false, false, false, true, true, true, true, true, true, true,
+																		true, true, true, true, false, false, false, true, false, true, true, true, true, true},
+															 /*Nº 9*/{	true, true, true, true, false, true, false, true, false, true, false, true, true, true,
+																		true, true, true, true, true, true, false, true, false, false, true, false, true, true},
+															 /*Nº 10*/{	true, true, true, true, true, true, true, true, false, true, false, true, false, false,
+																		true, true, true, true, true, true, true, true, true, false, true, false, false, false},
+															 /*Nº 11*/{	true, true, true, true, true, true, true, true, true, false, true, false, true, false,
+																		true, false, true, false, true, true, true, true, true, true, false, true, false, true} };
 
 bool arenaPattern1[ARENA_TILES] = { true, true, true, true, true, true, true, true, true, true,
 									true, true, true, true, true, true, true, true, true, true,
