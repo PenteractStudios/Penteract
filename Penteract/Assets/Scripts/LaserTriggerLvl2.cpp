3e84f1de7bac74c2c542f05330cc6a2d9aa11fb6
FrancPS <fportasol@gmail.com>
2021-10-12 20:31:29 +0200

    LEVEL 2 UPDATE - BOSS ARENA LASERS

---
+++ Penteract/Assets/Scripts/LaserTriggerLvl2.cpp
@@ -0,0 +1,28 @@
+#include "LaserTriggerLvl2.h"
+#include "GameObject.h"
+#include "GameplaySystems.h"
+#include "MovingLasers.h"
+
+EXPOSE_MEMBERS(LaserTriggerLvl2) {
+    MEMBER(MemberType::GAME_OBJECT_UID, laserUID),
+    MEMBER(MemberType::BOOL, SwitchOn)
+};
+
+GENERATE_BODY_IMPL(LaserTriggerLvl2);
+
+void LaserTriggerLvl2::Start() {
+    GameObject* laser = GameplaySystems::GetGameObject(laserUID);
+    laserScript = GET_SCRIPT(laser, MovingLasers);
+}
+
+void LaserTriggerLvl2::Update() {
+	
+}
+
+void LaserTriggerLvl2::OnCollision(GameObject& collidedWith, float3 /*collisionNormal*/, float3 /*penetrationDistance*/, void* /*particle*/) {
+    if (collidedWith.name == "Fang" || collidedWith.name == "Onimaru") {
+        if (laserScript) (SwitchOn)? laserScript->TurnOn() : laserScript->TurnOff();
+        GameObject* ownerGo = &GetOwner();
+        if(ownerGo) ownerGo->Disable();
+    }
+}
