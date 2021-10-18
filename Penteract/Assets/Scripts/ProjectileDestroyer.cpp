381534a5f3836afb59dcce7a4f67fbc0cec3701f
magalenyo <magalenyo7@gmail.com>
2021-10-18 22:25:51 +0200

    Added floor collision with Duke's autoattack

---
+++ Penteract/Assets/Scripts/ProjectileDestroyer.cpp
@@ -0,0 +1,23 @@
+#include "ProjectileDestroyer.h"
+#include "GameplaySystems.h"
+
+EXPOSE_MEMBERS(ProjectileDestroyer) {
+};
+
+GENERATE_BODY_IMPL(ProjectileDestroyer);
+
+void ProjectileDestroyer::Start() {
+	
+}
+
+void ProjectileDestroyer::Update() {
+}
+
+void ProjectileDestroyer::OnCollision(GameObject& collidedWith, float3 collisionNormal, float3 penetrationDistance, void* particle) {
+	if (collidedWith.name == "DukeProjectile") {
+		if (!particle) return;
+		ComponentParticleSystem::Particle* p = (ComponentParticleSystem::Particle*)particle;
+		ComponentParticleSystem* pSystem = collidedWith.GetComponent<ComponentParticleSystem>();
+		if (pSystem && p) pSystem->KillParticle(p);
+	}
+}
