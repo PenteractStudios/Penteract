4a0900d786ef51cf5cb8a171b250d67faa2671c9
Devildrake <davidsierragonzalez@enti.cat>
2021-10-29 23:19:58 +0200

    Made necesary changes on UI to show the proper button to press on dialogues/tutorials/skippingCanvas

---
+++ Penteract/Assets/Scripts/UIInputTextChanger.cpp
@@ -0,0 +1,50 @@
+#include "UIInputTextChanger.h"
+
+#include "GameplaySystems.h"
+#include "GlobalVariables.h"
+#include "Components/UI/ComponentText.h"
+
+EXPOSE_MEMBERS(UIInputTextChanger) {
+	MEMBER(MemberType::STRING, keyboardText),
+	MEMBER(MemberType::STRING, gameControllerText)
+		
+};
+
+GENERATE_BODY_IMPL(UIInputTextChanger);
+
+
+
+void UIInputTextChanger::Start() {
+
+	textToModify = GetOwner().GetComponent<ComponentText>();
+
+	SetTextToKeyboard();
+
+}
+
+void UIInputTextChanger::Update() {
+
+	bool gamepad = GameplaySystems::GetGlobalVariable(globalUseGamepad, false) && Input::IsGamepadConnected(0);
+
+	if (lastSerializedGamepadOn) {
+		if (!gamepad) {
+			SetTextToKeyboard();
+		}
+	} else {
+		if (gamepad) {
+			SetTextToGamepad();
+		}
+	}
+	lastSerializedGamepadOn = gamepad;
+
+}
+
+void UIInputTextChanger::SetTextToKeyboard() {
+	if (!textToModify) return;
+	textToModify->SetText(keyboardText);
+}
+
+void UIInputTextChanger::SetTextToGamepad() {
+	if (!textToModify) return;
+	textToModify->SetText(gameControllerText);
+}
